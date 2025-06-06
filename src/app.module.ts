15e1466901486fcce6f69c04d0419a8b6e038772
Jean Lima <jeansantoslima17@gmail.com>
2025-06-06 18:55:00 -0300

    feat: configure AppModule with User, Auth, and Transaction modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './services/prisma.service';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { TransactionModule } from './modules/transaction/transaction.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, AuthModule, TransactionModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
