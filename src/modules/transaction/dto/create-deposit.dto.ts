549f6b823ef4ffec9f55546b4c199386a77ec664
Jean Lima <jeansantoslima17@gmail.com>
2025-06-07 12:13:47 -0300

    feat(transaction): add maximum limit validation to deposit and transfer DTOs

--- src/modules/transaction/dto/create-deposit.dto.ts
+++ src/modules/transaction/dto/create-deposit.dto.ts
@@ -1,7 +1,8 @@
-import { IsNumber, Min } from 'class-validator';
+import { IsNumber, Max, Min } from 'class-validator';
 
 export class CreateDepositDto {
   @IsNumber({}, {message: 'O valor deve ser um número'})
   @Min(0.01, {message: 'Valor mínimo de depósito é R$ 0,01'})
+  @Max(99999999.99, { message: 'Valor máximo permitido é R$ 99.999.999,99' })
   amount: number;
 }
--- src/modules/transaction/dto/create-transfer.dto.ts
+++ src/modules/transaction/dto/create-transfer.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber, Min, IsEmail } from 'class-validator';
+import { IsNumber, Min, IsEmail, Max } from 'class-validator';
 
 export class CreateTransferDto {
   @IsEmail({}, { message: 'O e-mail do destinatário deve ser válido' })
@@ -6,5 +6,6 @@ export class CreateTransferDto {
 
   @IsNumber({}, { message: 'O valor deve ser um número' })
   @Min(0.01, { message: 'Valor mínimo de transferência é R$ 0,01' })
+  @Max(99999999.99, { message: 'Valor máximo permitido é R$ 99.999.999,99' })
   amount: number;
 }
